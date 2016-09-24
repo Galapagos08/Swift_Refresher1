fff7a4d6f940291aa90ef893cd002a60df3c71be
Dan Esrey <danesrey@Dans-MacBook-Air.local>
2016-09-24 18:12:33 -0400

    Initial Commit

---
+++ Swift_Refresher/Swift_Refresher.h
@@ -0,0 +1,19 @@
+//
+//  Swift_Refresher.h
+//  Swift_Refresher
+//
+//  Created by Dan Esrey on 2016/24/09.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swift_Refresher.
+FOUNDATION_EXPORT double Swift_RefresherVersionNumber;
+
+//! Project version string for Swift_Refresher.
+FOUNDATION_EXPORT const unsigned char Swift_RefresherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_Refresher/PublicHeader.h>
+
+
